2d2a267537e76a465836ef61897717a879102374
Korver <xz031015031015@gmail.com>
2023-03-14 19:19:33 +0800

    0314_1919

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ result = os.system('whisper /home/korver/Downloads/c.mp4 --language Japanese' )
 print(result)'''
 from googletrans import Translator
 
-translator = Translator(service_urls=['translate.google.cn'])
-translator.raise_Exception = True
-text = '今日の天気は晴れです'
-result = translator.translate(text, src='ja', dest='zh-CN')
-
+translator = Translator()
+source_file = open('c.txt', 'r')
+source_contents = source_file.read()
+result = translator.translate(source_contents, src='ja', dest='zh-CN')
 print(result.text)
-
-
+target_file = open('ca.txt', 'w')
+target_file.write(result.text)
 
 # print('ch:', translator.translate('我覺得今天天氣不好', dest='zh-tw').text)
 
