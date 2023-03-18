2b10681090e600b7b56a5b7dba3a152d2c1aa960
Korver <xz031015031015@gmail.com>
2023-03-19 00:28:09 +0800

    complete ui setting

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 from video import get_srt_file, trans_to_chinese
+from caption import write_caption
 
 UPLOAD_FOLDER = './mp4'
 Directory_PATH = os.path.abspath(UPLOAD_FOLDER) + '/'
@@ -19,22 +20,23 @@ def upload_file():
         abs_path = Directory_PATH+filename
         get_srt_file(abs_path, lang, './srt_file')
         srt_file = filename.split('.')[0]+'.srt'
-        trans_srt_file = filename.split('.')[0] + 'a.srt'
-        trans_to_chinese(srt_file, trans_srt_file)
-        return redirect(url_for('show_filepath',
-                                filename=filename))
+        res_srt_file = trans_to_chinese(srt_file)
+        output = './result/'+filename
+        write_caption(res_srt_file, abs_path, output)
+        return redirect(url_for('show_filepath', filename=filename))
     return render_template('home.html')
 
 
 @app.route('/show_filepath/<filename>')
 def show_filepath(filename):
-    abs_path = Directory_PATH + filename
-    return render_template('file_path.html', file_path=abs_path, filename=filename)
+    output = os.path.abspath('./result/')+filename
+    return render_template('file_path.html', file_path=output, filename=filename)
 
 
 @app.route('/uploaded_file/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(Directory_PATH, filename)
+    output_dir = os.path.abspath('./result/')
+    return send_from_directory(output_dir, filename)
 
 
 @app.route('/')
--- video.py
+++ video.py
@@ -1,31 +1,19 @@
 import os
 from googletrans import Translator
 
-"""
-result = os.system('whisper ~/Downloads/ee.mp4 --language Japanese --task translate --output_dir "./srt_file"')
-translator = Translator()
-
-with open('./srt_file/ee.srt', 'r') as sf, open('./srt_file/eea.srt', 'w') as tf:
-    source_contents = sf.read()
-    target_contents = translator.translate(source_contents, src='en', dest='zh-TW')
-    tf.write(str(target_contents.text))
-
-print("translate over")
-"""
-
 
 def get_srt_file(input_mp4: str, language: str, output_dir: str):
     command = 'whisper {} --language {} --task translate --output_dir {}'.format(input_mp4, language, output_dir)
     os.system(command)
 
 
-def trans_to_chinese(raw_srt: str, ans_art: str):
+def trans_to_chinese(raw_srt: str):
     translator = Translator()
+    ans_srt = raw_srt.split('.')[0] + 'a.srt'
     sf_location = './srt_file/{}'.format(raw_srt)
-    tf_location = './srt_file/{}'.format(ans_art)
+    tf_location = './srt_file/{}'.format(ans_srt)
     with open(sf_location, 'r') as sf, open(tf_location, 'w') as tf:
         source_contents = sf.read()
         target_contents = translator.translate(source_contents, src='en', dest='zh-TW')
         tf.write(str(target_contents.text))
-    print("translate over")
-
+    return tf_location
